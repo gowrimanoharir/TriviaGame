0557b42c8c2cca3376ff59945f80a6c20dc6fde4
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-06-02 21:47:07 -0500

    fixed issues with multiple calls after 1st round

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,16 @@
-var play, questions, curQuestion, curCat, counter, 
-counterInterval, ansTimeout, holdThis;
+var play, counterInterval, ansTimeout, holdThis;
 
 
 var triviagame = {
-	//counter: 0, 
-	//counterInterval: null,
 	correct: 0,
 	incorrect: 0,
 	unanswered: 0,
+	counter: 0,
 	isTimedOut: null,
-	userSelection: null,
+	curCat: null,
+	questions: null,
+	curQuestion: null,
+	userSelection: null,	
 	//Object to store categories of trivia questions
 	gameQuestions: 
 	{	cat1: {
@@ -48,76 +49,76 @@ var triviagame = {
 		}, 
 	},
 	
-	//Initialize variables for each round of the game
-	gameInitialize: function(){
-		console.log('in gm initialize');
-		counterInterval=null;
-		ansTimeout=null;
-		counter=0;
-		questions=Object.keys(curCat);
-		curQuestion=null;
-		curQuestion= curCat[questions.pop()];
-		this.correct=0;
-		this.incorrect=0;
-		this.unanswered=0;
-		this.curQuestionInitialize();
-	},
-
 	//To display the start page with Category buttons
 	displayStartPg: function(){
 		holdThis=this;
-		questions=null;
+		holdThis.questions=null;
 		$('#js-question').html('Select a Category');
 		$('#js-answer').empty();
 		$('#js-answer').append('<button class=\'btn js-gmbtn\' value=\'cat1\'>Entertainment'+'</button>');		
 		$('#js-answer').append('<button class=\'btn js-gmbtn\' value=\'cat2\'>Technology'+'</button>');		
 		$('#js-answer').on('click', '.js-gmbtn', function(){
-			curCat=holdThis.gameQuestions[$(this).attr('value')];
+			holdThis.curCat=holdThis.gameQuestions[$(this).attr('value')];
 			holdThis.gameInitialize();
 		});
+
+	},
+
+	//Initialize variables for each round of the game
+	gameInitialize: function(){
+		$('#js-answer').off('click', '.js-gmbtn');
+		holdThis.counter=0;
+		holdThis.questions=Object.keys(holdThis.curCat);
+		holdThis.curQuestion=null;
+		holdThis.curQuestion= holdThis.curCat[holdThis.questions.pop()];
+		holdThis.correct=0;
+		holdThis.incorrect=0;
+		holdThis.unanswered=0;
+		holdThis.curQuestionInitialize();
 	},
 
 	/*This is to initialize and display the Question 
 	and options*/
 	curQuestionInitialize: function(){
-		console.log('in curquest initialize');
 		clearTimeout(ansTimeout);
-		clearInterval(counterInterval);
 		counterInterval=null;
 		ansTimeout=null;
-		counter=30; 
-		this.isTimedOut=false;
-		counterInterval=setInterval(holdThis.startCounter, 1000);
-		$('#js-question').html(curQuestion.question);
+		holdThis.counter=6; 
+		holdThis.isTimedOut=false;
+		counterInterval=setInterval(function()
+			{
+				holdThis.startCounter();
+			}, 1000);
+		$('#js-question').html(holdThis.curQuestion.question);
 		$('#js-answer').empty();
 		for (i=0; i<4; i++){
-			$('#js-answer').append('<p class=\'js-options\' value='+i+'>'+curQuestion.options[i]+'</p>');			
+			$('#js-answer').append('<p class=\'js-options\' value='+i+'>'+holdThis.curQuestion.options[i]+'</p>');			
 		}
 	},
 	
 	/*To display the answer page after each question 
 	is answered or timesout*/
 	calcNdisplay: function(){
-		console.log('in calc n display');
-		if (this.isTimedOut){
-			this.unanswered++;
+		clearInterval(counterInterval);
+		if (holdThis.isTimedOut){
+			holdThis.unanswered++;
 			$('#js-answer').empty();
-			$('#js-answer').append('<p class=\'anstxt\'>You ran out of time, it is '+curQuestion.answer+'</p>');
+			$('#js-answer').append('<p class=\'anstxt\'>You ran out of time, it is '+holdThis.curQuestion.answer+'</p>');
 			$('#js-answer').append('<img class=\'ansimg\'src=\'assets/images/wait.jpg\'>');
 		}
-		else if (this.userSelection===curQuestion.answer){
-			this.correct++;
+		else if (holdThis.userSelection===holdThis.curQuestion.answer){
+			holdThis.correct++;
 			$('#js-answer').empty();
-			$('#js-answer').append('<p class=\'anstxt\'>You are correct, it is '+curQuestion.answer+'</p>');
+			$('#js-answer').append('<p class=\'anstxt\'>You are correct, it is '+holdThis.curQuestion.answer+'</p>');
 			$('#js-answer').append('<img class=\'ansimg\'src=\'assets/images/win.gif\'>');
 		}
 		else{
-			this.incorrect++;
+			holdThis.incorrect++;
 			$('#js-answer').empty();
-			$('#js-answer').append('<p class=\'anstxt\'>You are incorrect it is '+curQuestion.answer+'</p>');
+			$('#js-answer').append('<p class=\'anstxt\'>You are incorrect it is '+holdThis.curQuestion.answer+'</p>');
 			$('#js-answer').append('<img class=\'ansimg\'src=\'assets/images/lose.jpg\'>');
 		}
-		this.changeQuestion();
+		holdThis.changeQuestion();
 		
 	},
 
@@ -125,17 +126,14 @@ var triviagame = {
 	the function to display the next question*/
 
 	changeQuestion: function(){
-		if(questions.length>0)
+		if(holdThis.questions.length>0)
 		{
-			console.log('in chng question next que');
-			curQuestion=curCat[questions.pop()];
+			holdThis.curQuestion=holdThis.curCat[holdThis.questions.pop()];
 			ansTimeout=setTimeout(function(){
-				console.log('i am in');
 				holdThis.curQuestionInitialize();
 			}, 5000);
 		}
 		else{
-			console.log('in chng question gameover');
 			ansTimeout=setTimeout(function(){
 				holdThis.gameOver();
 			}, 5000);
@@ -145,62 +143,43 @@ var triviagame = {
 
 	//To display the final score screen after the end of a round
 	gameOver: function(){
-		console.log('in gm over');
-		clearInterval(counterInterval);
-		clearTimeout(ansTimeout);
 		$('#js-question').html('Thanks for Playing!! Your scores are:');
 		$('#js-answer').empty();
-		$('#js-answer').append('<p class=\'anstxt\'>Correct Answers: '+this.correct+'</p>');		
-		$('#js-answer').append('<p class=\'anstxt\'>Incorrect Answers: '+this.incorrect+'</p>');		
-		$('#js-answer').append('<p class=\'anstxt\'>Unanswered Answers: '+this.unanswered+'</p>');		
-		$('#js-answer').append('<button class=\'btn js-agnbtn\'>Play Again'+'</button>');		
+		$('#js-answer').append('<p class=\'anstxt\'>Correct Answers: '+holdThis.correct+'</p>');		
+		$('#js-answer').append('<p class=\'anstxt\'>Incorrect Answers: '+holdThis.incorrect+'</p>');		
+		$('#js-answer').append('<p class=\'anstxt\'>Unanswered Answers: '+holdThis.unanswered+'</p>');		
+		$('#js-answer').append('<button class=\'btn js-agnbtn\'>Play Again'+'</button>');	
+		holdThis=null;	
 	},
 
 
 	//to decrease the counter and update time display
 	startCounter: function (){
-		console.log('in startcounter');
-		$('#js-countdn').html(counter);
-		counter--;
-		console.log((counter<0))
-		if(counter<0){
-			holdThis.stopCounter();
+		$('#js-countdn').html(holdThis.counter);
+		holdThis.counter--;
+		
+		if(holdThis.counter<=0){
+			holdThis.isTimedOut=true;
+			holdThis.calcNdisplay();
 		}
 	},
 
-	//to stop the counter once user answers or timed out
-	stopCounter: function (){
-		clearInterval(counterInterval);
-		console.log('cleared interval');
-		if (counter<=0)
-		{
-			holdThis.isTimedOut=true;
-			console.log('timeout'+holdThis.isTimedOut);
-		}
-		holdThis.calcNdisplay();
-	}
 };
 
 
 $(document).ready(function(){
-	var i=1;
-	
+
 	play=triviagame;
 	play.displayStartPg();
-	
 
-
+	//Get the user selection and call function to verify the results
 	$('#js-answer').on('click', '.js-options', function(){
-		console.log("this is click "+i);
-		play.userSelection=curQuestion.options[$(this).attr('value')];
-		console.log(play.userSelection);
-		play.stopCounter();
-		//play.calcNdisplay();
-		i++;
+		play.userSelection=play.curQuestion.options[$(this).attr('value')];
+		play.calcNdisplay();
 	});
 
+	//If user clicks play again load the start page again
 	$('#js-answer').on('click', '.js-agnbtn', function(){
-		play=triviagame;
 		play.displayStartPg();
 	});
 
