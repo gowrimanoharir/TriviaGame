8efc1208667c99b4cfb18c584d97067910e9a6dd
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-06-09 23:38:08 -0500

    changed background images and other styles, also added description text in results page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,21 +17,67 @@ var triviagame = {
 	//Object to store categories of trivia questions
 	gameQuestions: 
 	{	cat1: {
-			CQ1: {
-				question: "Cquestion 1",
-				options: ["C1-opt1", "C1-opt2", "C1-opt3", "C1-opt4"],
-				answer: "C1-opt1"
+			C1Q1: {
+				question: "Which whale has a tusk that can grow upto 10ft?",
+				options: ["Beluga", "Narwhal", "Orca", "Sperm"],
+				answer: "Narwhal",
+				description: "Narwhal is a medium-sized whale that lives-year round in Arctic waters, only the male possess the Tusk" 
 			},
-			CQ2: {
-			question: "Cquestion 2",
-			options: ["C2-opt1", "C2-opt2", "C2-opt3", "C2-opt4"],
-			answer: "C2-opt1"
+			C1Q2: {
+				question: "Which of the below is not a marsupial aka mammal without a pouch?",
+				options: ["Kangaroo", "Possum", "Koala", "Lemur"],
+				answer: "Lemur",
+				description: "Lemurs are native to Madagascar popularized by the character of King Julien in the namesake animated movie"
 			},
-			CQ3: {
-			question: "Cquestion 3",
-			options: ["C3-opt1", "C3-opt2", "C3-opt3", "C3-opt4"],
-			answer: "C3-opt1"
-			} 
+			C1Q3: {
+				question: "How many approximate kills does a lion in the wild make?",
+				options: ["Twenty", "Seventy", "Fifty Two", "Forty"],
+				answer: "Twenty",
+				description: "On average, a single lion will kill 15-20 large herbivores a year. Lions hunt in a group mostly in the evening or early morning"
+			},
+			C1Q4: {
+				question: "Snakes are...",
+				options: ["Herbivore", "Omnivore", "Carnivore", "Cannibal"],
+				answer: "Carnivore",
+				description: "all varieties of snakes are strictly carnivores, meaning that they feed only on other animals such as rodents, lizards, and even the eggs of other reptiles"
+			},
+			C1Q5: {
+				question: "Group of Owls are called?",
+				options: ["Congress", "Senate", "Parliament", "House"],
+				answer: "Parliament",
+				description: "They are called a “parliament” as they have long been considered to be of a wise disposition"
+			},
+			C1Q6: {
+				question: "Which is the first domesticated animal by Humans?",
+				options: ["Goats", "Dogs", "Cows", "Horses"],
+				answer: "Goats",
+				description: "Goats were probably the first animals to be domesticated, followed closely by sheep about 10,000 years ago"
+			},
+			C1Q7: {
+				question: "Which of these is a real animal?",
+				options: ["Himalayan Yeti", "Chupacabra", "Tasmanian Devil", "Sphynx"],
+				answer: "Tasmanian Devil",
+				description: "Tasmanian devil is a carnivorous marsupial, once native to mainland Australia and now found in the wild only on the island state of Tasmania"
+			},
+			C1Q8: {
+				question: "Which is the Fastest dog in the World?",
+				options: ["Doberman", "Greyhound", "Jack Russel Terrier", "Dalmation"],
+				answer: "Greyhound",
+				description: "Greyhound's can run upto 43MPH and primarily known for their racing background"
+			},
+			C1Q9: {
+				question: "Which of the below is not an animal?",
+				options: ["Sea Pigs", "Sea Cows", "Sea Lions", "Sea Monkeys"],
+				answer: "Sea Monkeys",
+				description: "Sea Monkeys is a brand name for brine shrimp pet kit"
+			},
+			C1Q10: {
+				question: "Tweety and Big bird from Sesame street is based on which actual bird?",
+				options: ["Conure", "Cockatiel", "Canary", "Condor"],
+				answer: "Canary",
+				description: "Canary is a small songbird in the finch family originating from Macronesian Islands"
+			}	
+
 		}, 
 		cat2: {
 			DQ1: {
@@ -52,17 +98,34 @@ var triviagame = {
 		}, 
 		cat3: {
 			EQ1: {
-				question: "Dquestion 1",
+				question: "Equestion 1",
 				options: ["D1-opt1", "D1-opt2", "D1-opt3", "D1-opt4"],
 				answer: "D1-opt1"
 			},
 			EQ2: {
-				question: "Dquestion 2",
+				question: "Equestion 2",
 				options: ["D2-opt1", "D2-opt2", "D2-opt3", "D2-opt4"],
 				answer: "D2-opt1"
 			},
 			EQ3: {
-				question: "question 3",
+				question: "Equestion 3",
+				options: ["D3-opt1", "D3-opt2", "D3-opt3", "D3-opt4"],
+				answer: "D3-opt1"
+			} 
+		},
+		cat4: {
+			EQ1: {
+				question: "Fquestion 1",
+				options: ["D1-opt1", "D1-opt2", "D1-opt3", "D1-opt4"],
+				answer: "D1-opt1"
+			},
+			EQ2: {
+				question: "Equestion 2",
+				options: ["D2-opt1", "D2-opt2", "D2-opt3", "D2-opt4"],
+				answer: "D2-opt1"
+			},
+			EQ3: {
+				question: "Fquestion 3",
 				options: ["D3-opt1", "D3-opt2", "D3-opt3", "D3-opt4"],
 				answer: "D3-opt1"
 			} 
@@ -72,12 +135,13 @@ var triviagame = {
 	//To display the start page with Category buttons
 	displayStartPg: function(){
 		this.questions=null;
-		$('#js-timetxt').html('');
-		$('#js-question').html('');
+		$('#js-timetxt').empty();
+		$('#js-question').empty();
 		$('#js-answer').empty();
-		$('#js-answer').append('<button class=\'btn js-gmbtn center-block catbtn\' value=\'cat1\'>Entertainment'+'</button>');		
+		$('#js-answer').append('<button class=\'btn js-gmbtn center-block catbtn\' value=\'cat1\'>Animal World'+'</button>');		
 		$('#js-answer').append('<button class=\'btn js-gmbtn center-block catbtn\' value=\'cat2\'>Technology'+'</button>');	
 		$('#js-answer').append('<button class=\'btn js-gmbtn center-block catbtn\' value=\'cat3\'>Sports'+'</button>');		
+		$('#js-answer').append('<button class=\'btn js-gmbtn center-block catbtn\' value=\'cat4\'>Entertainment'+'</button>');	
 		$('#js-answer').on('click', '.js-gmbtn', function(e){
 			this.curCat=this.gameQuestions[e.target.value];
 			this.gameInitialize();
@@ -96,7 +160,8 @@ var triviagame = {
 		this.incorrect=0;
 		this.unanswered=0;
 		this.curQuestionInitialize();
-		$('#js-timetxt').html('Time Remaining: <span id="js-countdn">15</span>');
+		$('#js-timetxt').html('Time Remaining: <span id="js-countdn" class="countdn">30</span>');
+		
 	},
 
 	/*This is to initialize and display the Question 
@@ -105,7 +170,7 @@ var triviagame = {
 		clearTimeout(ansTimeout);
 		counterInterval=null;
 		ansTimeout=null;
-		this.counter=15; 
+		this.counter=30; 
 		this.isTimedOut=false;
 		$('#js-countdn').html(this.counter);
 		counterInterval=setInterval(function()
@@ -126,20 +191,20 @@ var triviagame = {
 		if (this.isTimedOut){
 			this.unanswered++;
 			$('#js-answer').empty();
-			$('#js-answer').append('<p class=\'anstxt text-center\'>You ran out of time, it is '+this.curQuestion.answer+'</p>');
-			$('#js-answer').append('<img class=\'ansimg center-block\'src=\'assets/images/wait.jpg\'>');
+			$('#js-answer').append('<p class=\'anstxt unanswered text-center\'>Ran out of time, it is <b>'+this.curQuestion.answer+'</b></p>');
+			$('#js-answer').append('<p class=\'text-center ansbk\'>'+this.curQuestion.description+'</p>');
 		}
 		else if (this.userSelection===this.curQuestion.answer){
 			this.correct++;
 			$('#js-answer').empty();
-			$('#js-answer').append('<p class=\'anstxt text-center\'>You are correct, it is '+this.curQuestion.answer+'</p>');
-			$('#js-answer').append('<img class=\'ansimg center-block\'src=\'assets/images/win.gif\'>');
+			$('#js-answer').append('<p class=\'anstxt correct text-center\'>YES!!! it is <b>'+this.curQuestion.answer+'</b></p>');
+			$('#js-answer').append('<p class=\'text-center ansbk\'>'+this.curQuestion.description+'</p>');
 		}
 		else{
 			this.incorrect++;
 			$('#js-answer').empty();
-			$('#js-answer').append('<p class=\'anstxt text-center\'>You are incorrect it is '+this.curQuestion.answer+'</p>');
-			$('#js-answer').append('<img class=\'ansimg center-block\'src=\'assets/images/lose.jpg\'>');
+			$('#js-answer').append('<p class=\'anstxt incorrect text-center\'>Wrong, it is <b>'+this.curQuestion.answer+'</b></p>');
+			$('#js-answer').append('<p class=\'text-center ansbk\'>'+this.curQuestion.description+'</p>');
 		}
 		this.changeQuestion();
 		
@@ -166,30 +231,32 @@ var triviagame = {
 
 	//To display the final score screen after the end of a round
 	gameOver: function(){
-		$('#js-timetxt').html('Thanks for Playing!!');
-		$('#js-question').html('Your scores are:');
+		$('#js-timetxt').html('');
+		$('#js-question').html('');
 		$('#js-answer').empty();
 		$('#js-answer').append('<p class=\'anstxt text-center\'>Correct Answers: '+this.correct+'</p>');		
 		$('#js-answer').append('<p class=\'anstxt text-center\'>Incorrect Answers: '+this.incorrect+'</p>');		
-		$('#js-answer').append('<p class=\'anstxt text-center\'>Unanswered Answers: '+this.unanswered+'</p>');		
-		$('#js-answer').append('<button class=\'btn js-agnbtn center-block catbtn\'>Play Again'+'</button>');	
+		$('#js-answer').append('<p class=\'anstxt text-center\'>Unanswered: '+this.unanswered+'</p>');		
+		$('#js-answer').append('<button class=\'btn js-agnbtn center-block agnbtn catbtn\'>Play Again'+'</button>');	
 	},
 
 
 	//to decrease the counter and update time display
 	startCounter: function (){
+		this.counter--;
+		if(this.counter<0){
+			this.isTimedOut=true;
+			this.calcNdisplay();
+			return;
+		}
+
 		if(this.counter<10){
 			$('#js-countdn').html('0'+this.counter);
 		}
 		else{
 			$('#js-countdn').html(this.counter);
 		}
-		this.counter--;
 		
-		if(this.counter<=0){
-			this.isTimedOut=true;
-			this.calcNdisplay();
-		}
 	},
 
 };
